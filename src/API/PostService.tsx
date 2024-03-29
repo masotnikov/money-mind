d7e2ef3b84a479a3c44439fe2d59596e92d761c2
masotnikov <m4ksim825@ya.ru>
2024-03-29 23:52:21 +0300

    Create custom hook useFetching

--- src/API/PostService.tsx
+++ src/API/PostService.tsx
@@ -1,3 +1,8 @@
-export default class PostService {
+import axios from "axios";
+import {ITransaction} from "../@types/types";
 
+export default class PostService {
+ static async getTransactions (){
+    return await axios.get(`http://localhost:3000/transactions`);
+ }
 }
