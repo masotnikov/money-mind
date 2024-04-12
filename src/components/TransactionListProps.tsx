92c9bc5d4ef21bc73b5f5ffd1bf7959f0e450a80
masotnikov <m4ksim825@ya.ru>
2024-04-12 18:51:16 +0300

    добавил компонент со списком свойст транзакции TransactionListProps.tsx

---
+++ src/components/TransactionListProps.tsx
@@ -0,0 +1,29 @@
+import cl from "./TransactionItem/TransactionItem.module.scss";
+import React, {FC} from "react";
+import {ITransaction} from "../@types/types";
+
+interface ITransactionProps {
+  transaction: ITransaction;
+}
+
+const TransactionListProps:FC<ITransactionProps> = ({transaction}) => {
+  const {type, category, amount, description, date} = transaction;
+
+  return (
+    <ul>
+      <li>Тип:
+        <span className={`${cl.defaultBackground} ${type === 'расход'
+          ? cl.redBackground
+          : cl.greenBackground}`}>
+          {type}
+          </span>
+      </li>
+      <li>Категория: {category}</li>
+      <li>Сумма: {amount}</li>
+      <li>Дата: {date}</li>
+      <li>Описание: {description}</li>
+    </ul>
+  )
+}
+
+export default TransactionListProps;
