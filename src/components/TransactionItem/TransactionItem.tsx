8dce6aa0bc0c599fd378baf8cd7ab70a852c527c
masotnikov <m4ksim825@ya.ru>
2024-04-13 10:33:18 +0300

    добавил ф-ю по удалению транзакции

--- src/components/TransactionItem/TransactionItem.tsx
+++ src/components/TransactionItem/TransactionItem.tsx
@@ -4,18 +4,25 @@ import MyButton from "../UI/button/MyButton";
 import cl from './TransactionItem.module.scss'
 import {useNavigate} from "react-router-dom";
 import TransactionListProps from "../TransactionListProps";
+import {useSoftDeleteTransactionMutation} from "../../API/TransactionService";
 
 interface TransactionItemProps {
   transaction: ITransaction;
-  removeTransaction: (id: number) => void;
 }
 
 
-const TransactionItem: FC<TransactionItemProps> = ({transaction, removeTransaction}) => {
+const TransactionItem: FC<TransactionItemProps> = ({transaction}) => {
 
-  const handleClickRemove = () => {
-    removeTransaction(transaction.id)
-  }
+  const [updateTransaction] = useSoftDeleteTransactionMutation()
+  const handleRemove = async () => {
+    // Отправляем запрос на сервер для обновления транзакции с флагом "удалено"
+    const { error } : any = await updateTransaction(transaction.id);
+    if (error) {
+      console.error('Произошла ошибка при обновлении транзакции', error);
+      return;
+    }
+    console.log('Транзакция успешно обновлена');
+  };
 
   const navigate = useNavigate();
 
@@ -23,7 +30,7 @@ const TransactionItem: FC<TransactionItemProps> = ({transaction, removeTransacti
     <div className={cl.root}>
       <TransactionListProps transaction={transaction}/>
       <MyButton onClick={() => navigate(`/details/${transaction.id}`)}>Подробнее</MyButton>
-      <img onClick={handleClickRemove} className={cl.closeIcon} src="/close.png"
+      <img onClick={handleRemove} className={cl.closeIcon} src="/close.png" title="удалить транзакцию"
            alt="remove-transaction"/>
       <hr style={{marginTop: 10, marginBottom: 5}}/>
     </div>
