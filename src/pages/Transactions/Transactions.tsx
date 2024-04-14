dbf13c244de270f1268f0f6569ee625909752266
masotnikov <m4ksim825@ya.ru>
2024-04-14 12:12:07 +0300

    .

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -4,31 +4,32 @@ import {useGetAllTransactionsQuery} from "../../API/TransactionService";
 import {useState} from "react";
 import {useTransactions} from "../../hooks/useTransactions";
 import TransactionFilter from "../../components/TransactionFilter/TransactionFilter";
+import MyButton from "../../components/UI/button/MyButton";
 
 export interface IFilter {
     sort: string;
     query: string;
   }
 
+const sortOptions = [
+  { value: '', name: 'Сортировать по: ', disabled: true },
+  { value: '', name: 'По умолчанию' },
+  { value: 'доход', name: '- по доходу' },
+  { value: 'расход', name: '- по расходу' },
+  { value: '', name: 'Категориям: ', disabled: true },
+  { value: 'Продукты', name: '- продукты'},
+  { value: 'Развлечения', name: '- развлечения' },
+  { value: 'Другое', name: '- другое' },
+]
+
+
 const Transactions = () => {
 
-  const {data: transactions, isLoading, error} = useGetAllTransactionsQuery(10);
+  const {data: transactions, isLoading, error : transactionError} = useGetAllTransactionsQuery(10);
   const [filter, setFilter] = useState<IFilter>({sort: '', query: ''});
   const searchedAndSortedTransactions = useTransactions(transactions, filter.query, filter.sort);
 
 
-
-  const sortOptions = [
-    { value: '', name: 'Сортировать по: ', disabled: true },
-    { value: '', name: 'По умолчанию' },
-    { value: 'доход', name: '- по доходу' },
-    { value: 'расход', name: '- по расходу' },
-    { value: '', name: 'Категориям: ', disabled: true },
-    { value: 'Продукты', name: '- продукты'},
-    { value: 'Развлечения', name: '- развлечения' },
-    { value: 'Другое', name: '- другое' },
-
-  ]
 
   if (isLoading) {
     return <Loader/>
@@ -40,8 +41,9 @@ const Transactions = () => {
         transactions={searchedAndSortedTransactions}
         title={"Все транзакции"}>
         <TransactionFilter filter={filter} setFilter={setFilter} sortOptions={sortOptions}/>
+        <MyButton>Добавить транзакцию</MyButton>
       </TransactionList>
-      {error && <h1>Произошла ошибка</h1>}
+      {transactionError && <h1>Произошла ошибка</h1>}
     </>
   )
 }
