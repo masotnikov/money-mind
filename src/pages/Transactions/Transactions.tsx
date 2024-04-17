e2bc891f07f8d085edbc0ef8a568c1d61f5de63b
masotnikov <m4ksim825@ya.ru>
2024-04-17 13:39:53 +0300

    добавил форму для добавления транзакции

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -5,30 +5,36 @@ import {useState} from "react";
 import {useTransactions} from "../../hooks/useTransactions";
 import TransactionFilter from "../../components/TransactionFilter/TransactionFilter";
 import MyButton from "../../components/UI/button/MyButton";
+import MyModal from "../../components/UI/modal/MyModal";
+import AddTransactionForm from "../../components/AddTrasnsactionForm/AddTransactionForm";
 
 export interface IFilter {
-    sort: string;
-    query: string;
-  }
+  sort: string;
+  query: string;
+}
 
 const sortOptions = [
-  { value: '', name: 'Сортировать по: ', disabled: true },
-  { value: '', name: 'По умолчанию' },
-  { value: 'доход', name: '- по доходу' },
-  { value: 'расход', name: '- по расходу' },
-  { value: '', name: 'Категориям: ', disabled: true },
-  { value: 'Продукты', name: '- продукты'},
-  { value: 'Развлечения', name: '- развлечения' },
-  { value: 'Другое', name: '- другое' },
+  {value: '', name: 'Сортировать по: ', disabled: true},
+  {value: '', name: 'По умолчанию'},
+  {value: 'доход', name: '- по доходу'},
+  {value: 'расход', name: '- по расходу'},
+  {value: '', name: 'Категориям: ', disabled: true},
+  {value: 'Продукты', name: '- продукты'},
+  {value: 'Развлечения', name: '- развлечения'},
+  {value: 'Другое', name: '- другое'},
 ]
 
 
 const Transactions = () => {
 
-  const {data: transactions, isLoading, error : transactionError} = useGetAllTransactionsQuery(10);
+  const {data: transactions, isLoading, error: transactionError} = useGetAllTransactionsQuery(10);
   const [filter, setFilter] = useState<IFilter>({sort: '', query: ''});
+  const [modal, setModal] = useState<boolean>(false);
   const searchedAndSortedTransactions = useTransactions(transactions, filter.query, filter.sort);
-
+  //
+  const handleCloseModal = (): void => {
+    setModal(false)
+  }
 
 
   if (isLoading) {
@@ -37,11 +43,14 @@ const Transactions = () => {
 
   return (
     <>
+      <MyModal modal={modal} setModal={setModal}>
+        <AddTransactionForm onClose={handleCloseModal}/>
+      </MyModal>
       <TransactionList
         transactions={searchedAndSortedTransactions}
         title={"Все транзакции"}>
         <TransactionFilter filter={filter} setFilter={setFilter} sortOptions={sortOptions}/>
-        <MyButton>Добавить транзакцию</MyButton>
+        <MyButton onClick={() => setModal(true)}>Добавить транзакцию</MyButton>
       </TransactionList>
       {transactionError && <h1>Произошла ошибка</h1>}
     </>
