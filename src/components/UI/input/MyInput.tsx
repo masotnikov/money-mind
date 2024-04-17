6cfabb6c4a17ace14162208d7ef07d07097d4dad
masotnikov <m4ksim825@ya.ru>
2024-04-17 13:41:05 +0300

    .

--- src/components/UI/input/MyInput.tsx
+++ src/components/UI/input/MyInput.tsx
@@ -4,7 +4,7 @@ interface IInputProps extends InputHTMLAttributes<HTMLInputElement>{}
 
 const MyInput: FC<IInputProps> = (props) => {
   return (
-    <input className={cl.MyInput} {...props} type="text"/>
+    <input className={cl.MyInput} {...props}/>
   )
 }
 
