3c8778490f28e6bf984fb82ae1d8ae87f2a565a7
masotnikov <m4ksim825@ya.ru>
2024-05-01 15:53:46 +0300

    отрефакторил код

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,38 +2,35 @@ import cl from './Main.module.scss'
 import {FC} from "react";
 import TransactionList from "../../components/TransactionList/TransactionList";
 import Loader from "../../components/UI/loader/Loader";
-import {useGetAllTransactionsQuery} from "../../API/TransactionService";
-import {ITransaction} from "../../@types/types";
-import {useGetCurrentBalanceQuery} from "../../API/BalanceService";
+import {useGetAllTransactionsQuery, useGetBalanceAndExpensesQuery} from "../../API/TransactionService";
 
 
 // @ts-ignore
 const Main: FC = () => {
-  // const balance = 20000
- // @ts-ignore
-  const {data: transactions, isLoading, error} = useGetAllTransactionsQuery();
   // @ts-ignore
-  const {data: balance} = useGetCurrentBalanceQuery();
- //
-  if (isLoading) {
+  const {data: transactionsData, transactionsLoading, error: transactionsError} = useGetAllTransactionsQuery();
+  // @ts-ignore
+  const {data: balanceAndExpensesData, isLoading: balanceAndExpensesLoading} = useGetBalanceAndExpensesQuery();
+
+  if (transactionsLoading || balanceAndExpensesLoading) {
     return <Loader/>
   }
 
-  const lastTransactions: ITransaction[] = [...transactions].slice(0,3);
+const lastTransactions = transactionsData?.slice(0, 3);
 
 
   return (
     <>
       <div className={cl.root}>
         <h2>Текущий баланс</h2>
-        <div className={cl.balanceAmount}>{balance}</div>
+        <div className={cl.balanceAmount}>{balanceAndExpensesData?.balance ?? 'Баланс недоступен'}</div>
         <hr/>
         <h2>Расходы</h2>
-        <div>8000</div>
+        <div>{balanceAndExpensesData?.expenses ?? 'Расходы недоступны'}</div>
       </div>
       <div>
         <TransactionList title="Последние транзакции" transactions={lastTransactions}></TransactionList>
-        {error && <h2 style={{textAlign: 'center'}}>Извините, произошла ошибка</h2>}
+        {transactionsError && <h2 style={{textAlign: 'center'}}>Извините, произошла ошибка</h2>}
       </div>
     </>
 
