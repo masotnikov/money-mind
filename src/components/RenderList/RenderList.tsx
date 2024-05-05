83bc91d4932c3907f7a6917c6f219db8392a39f8
masotnikov <m4ksim825@ya.ru>
2024-05-06 01:10:08 +0300

    Создал модульный компонент для рендеринга списков

---
+++ src/components/RenderList/RenderList.tsx
@@ -0,0 +1,38 @@
+import React, {FC} from "react";
+
+
+// interface RenderListProps<T> {
+//   title: string;
+//   emptyMessage: string;
+//   children?: React.ReactNode;
+//   RenderItemComponent: ComponentType<{ item: T }>;
+//   renderData: T[];
+// }
+
+
+const RenderList: FC<any> = (
+  {
+    children,
+    renderData,
+    title,
+    emptyMessage,
+    RenderItemComponent
+  }
+) => {
+
+  return (
+    <div>
+      <h3 style={{textAlign: "center", marginBottom: 5}}>{title}</h3>
+      {children}
+      {renderData?.length === 0 && <h2 style={{textAlign: "center"}}>{emptyMessage}</h2>}
+      <div>
+        {renderData?.map((item: any) => (
+          <RenderItemComponent key={item.id} item={item}></RenderItemComponent>
+        ))}
+      </div>
+    </div>
+  );
+
+};
+
+export default RenderList;
