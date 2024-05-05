881c74f53503f756d6a56caca233b806ced750c0
masotnikov <m4ksim825@ya.ru>
2024-05-06 01:10:21 +0300

    .

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { transactionAPI } from '../API/TransactionService'
+import {configureStore} from '@reduxjs/toolkit';
+import {transactionAPI} from '../API/TransactionService'
+import {goalsAPI} from "../API/GoalsService";
 
 const store = configureStore({
   reducer: {
     [transactionAPI.reducerPath]: transactionAPI.reducer,
+    [goalsAPI.reducerPath]: goalsAPI.reducer,
     // Другие редукторы вашего приложения, если есть
   },
   // Добавьте middleware для обработки запросов
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     transactionAPI.middleware,
+    goalsAPI.middleware
   ),
 });
 
