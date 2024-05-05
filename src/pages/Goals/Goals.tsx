d76e98a9a3892920c504c05e2937e07743c722a1
masotnikov <m4ksim825@ya.ru>
2024-05-06 01:07:51 +0300

    создал страницу Goals.tsx

--- src/pages/Goals/Goals.tsx
+++ src/pages/Goals/Goals.tsx
@@ -1,8 +1,30 @@
-
+import cl from './Goals.module.scss'
+import MyButton from "../../components/UI/button/MyButton";
+import {useState} from "react";
+import {useGetAllGoalsQuery} from "../../API/GoalsService";
+import Loader from "../../components/UI/loader/Loader";
+import RenderList from "../../components/RenderList/RenderList";
+import goalItem from "../../components/GoalItem/GoalItem";
 
 const Goals = () => {
+  const [modal, setModal] = useState<boolean>(false);
+
+  // @ts-ignore
+  const {data: goals, isLoading: goalsLoading, error: goalsError} = useGetAllGoalsQuery();
+
+  if (goalsLoading) {
+    return <Loader/>
+  }
+
   return (
-    <h2>Цели</h2>
+    <div className={cl.root}>
+      <MyButton>Создайте цель</MyButton>
+      <RenderList
+        emptyMessage={"Цели отсутствуют..."}
+        renderData={goals}
+        RenderItemComponent={goalItem}
+      ></RenderList>
+    </div>
   )
 }
 
