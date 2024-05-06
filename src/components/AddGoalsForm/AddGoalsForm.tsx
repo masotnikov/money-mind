2b842439f7160980c1d2010f26bfd4b563df4cb4
masotnikov <m4ksim825@ya.ru>
2024-05-06 19:26:21 +0300

    Создал функционал добавления цели

--- src/components/AddGoalsForm/AddGoalsForm.tsx
+++ src/components/AddGoalsForm/AddGoalsForm.tsx
@@ -1,10 +1,57 @@
+import React, {FC} from "react";
+import MyInput from "../UI/input/MyInput";
+import cl from "./AddGoals.module.scss"
+import MyButton from "../UI/button/MyButton";
+import {IGoal} from "../../@types/types";
+import {useAddNewGoalMutation} from "../../API/GoalsService";
+import {useForm} from "react-hook-form";
 
+interface IAddGoalsForm {
+  onClose: () => void
+}
 
-const AddGoalsForm = () => {
+const AddGoalsForm: FC<IAddGoalsForm> = ({onClose}) => {
+  const [addNewGoalMutation] = useAddNewGoalMutation();
+  const {handleSubmit, reset, setValue, register} = useForm<IGoal>()
+  const addNewGoal = async (goal: IGoal) => {
+    await addNewGoalMutation(goal);
+    onClose();
+  }
+
+  const onSubmit = (data: IGoal) => {
+    addNewGoal(data);
+    reset();
+  }
+
   return (
-    <div>
-
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className={cl.root}>
+      <MyInput
+        name="goalTitle"
+        type="text"
+        placeholder="Название цели"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setValue("title", e.target.value);
+        }}
+      ></MyInput>
+      <MyInput
+        name="goalDescription"
+        type="text"
+        placeholder="Описание цели"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setValue("description", e.target.value);
+        }}
+      ></MyInput>
+      <MyInput
+        name="goalAmount"
+        type="number"
+        placeholder="Необходимая сумма"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setValue("amount", +e.target.value);
+        }}
+        register={register("amount", {required: true})}
+      ></MyInput>
+      <MyButton type="submit">Добавить</MyButton>
+    </form>
   )
 }
 
