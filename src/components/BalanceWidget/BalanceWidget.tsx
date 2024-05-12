81076bc7c1c828d953cab61b6148a4c83253b522
masotnikov <m4ksim825@ya.ru>
2024-05-12 15:36:37 +0300

    переработал BalanceWidget.tsx

--- src/components/BalanceWidget/BalanceWidget.tsx
+++ src/components/BalanceWidget/BalanceWidget.tsx
@@ -1,64 +1,28 @@
-import React from "react";
-import {useAddNewTransactionMutation, useGetBalanceAndExpensesQuery} from "../../API/TransactionService";
-import Loader from "../UI/loader/Loader";
+import React, {useState} from "react";
 import cl from './BalanceWidget.module.scss'
-import MyButton from "../UI/button/MyButton";
-import MyInput from "../UI/input/MyInput";
-import {useForm} from "react-hook-form";
-import {ITransaction} from "../../@types/types";
+import BookmarkList from "../BookmarkList/BookmarkList";
+import CurrentAccount from "../CurrentAccount/CurrentAccount";
+import SavingsAccount from "../SavingsAccount/SavingsAccount";
+
+const CURRENT_ACCOUNT: string = 'Текущий счёт';
+const SAVING_ACCOUNT: string = 'Накопительный счёт';
+
+const bookmarks = [
+  {name: CURRENT_ACCOUNT, component: <CurrentAccount/>},
+  {name: SAVING_ACCOUNT, component: <SavingsAccount/>}
+];
 
 const BalanceWidget = () => {
-// @ts-ignore
-  const {data: balanceAndExpensesData, isLoading: balanceAndExpensesLoading} = useGetBalanceAndExpensesQuery();
-  const {handleSubmit, setValue, reset, register} = useForm<ITransaction>();
-  const [replenishBalance] = useAddNewTransactionMutation();
+  const [activeTab, setActiveTab] = useState<number>(0);
 
-  if (balanceAndExpensesLoading) {
-    return <Loader/>
-  }
-
-  const addNewTransaction = async (data: ITransaction) => {
-    data.type = "Доход";
-    data.category = "Другое";
-    data.description = "Пополнение счёта";
-    data.date = "2024-03-26";
-    await replenishBalance(data);
-  }
-
-  const onSubmit = (data: ITransaction) => {
-    addNewTransaction(data);
-    reset();
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
   }
 
   return (
     <div className={cl.root}>
-      <h2>Текущий баланс</h2>
-      <div style={{display: "flex"}}>
-        <span style={{padding: "0 10px"}}>{balanceAndExpensesData?.balance ?? 'Баланс недоступен'}</span>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <MyInput
-            name="replenishBalance"
-            style={{width: 130, verticalAlign: "top", borderRadius: 15}}
-            type="number"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setValue("amount", +e.target.value);
-            }}
-            register={register("amount", {
-              required: "Обязательное поле",
-              min: {
-                value: 1,
-                message: "Число должно быть больше или равно 1"
-              },
-            })}
-          ></MyInput>
-          <MyButton type="submit" style={{verticalAlign: "top", marginRight: 5}}>Пополнить</MyButton>
-        </form>
-      </div>
-      <hr/>
-      <h2>Расходы</h2>
-      <div>
-        <span style={{padding: "0 10px"}}>{balanceAndExpensesData?.expenses ?? 'Расходы недоступны'}</span>
-      </div>
+      <BookmarkList tabs={bookmarks} activeTab={activeTab} onTabClick={handleTabClick}/>
+      {bookmarks[activeTab].component}
     </div>
   )
 }
