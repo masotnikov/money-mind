3cc7d511ed944560291d41efa13a48e75e448d33
masotnikov <m4ksim825@ya.ru>
2024-05-12 15:22:13 +0300

    привязал прогрессбар к накопительному счёту

--- src/components/GoalItem/GoalItem.tsx
+++ src/components/GoalItem/GoalItem.tsx
@@ -3,6 +3,7 @@ import React, {FC} from "react";
 import cl from './GoalItem.module.scss'
 import ProgressBar from "../UI/progressBar/ProgressBar";
 import {useSoftDeleteGoalMutation} from "../../API/GoalsService";
+import {useGetBalanceAndExpensesQuery} from "../../API/TransactionService";
 
 interface IGoalItemProps {
   item: IGoal;
@@ -10,12 +11,14 @@ interface IGoalItemProps {
 
 
 const GoalItem: FC<IGoalItemProps> = ({item: goal}) => {
-  const {title, amount, status, description, progress} = goal
   const [updateGoal] = useSoftDeleteGoalMutation();
+  // @ts-ignore
+  const {data: balance} = useGetBalanceAndExpensesQuery();
+
 
   const handleRemove = async () => {
     // @ts-ignore
-    const {error} = await updateGoal(goal.id);
+    const {error} = await updateGoal(goal?.id);
     if (error) {
       console.log(error, 'Произошла ошибка при обновлении цели');
       return
@@ -23,18 +26,27 @@ const GoalItem: FC<IGoalItemProps> = ({item: goal}) => {
     console.log('Цель успешно обновлена');
   }
 
+  const calculateProgress = () => {
+    let progressInPercentage: number = 0;
+    if (balance?.saving && goal?.amount) {
+      progressInPercentage = (balance.saving / goal.amount) * 100
+    }
+    return Math.ceil(progressInPercentage)
+  }
+
+  const progress : number = calculateProgress();
 
   return (
     <div className={cl.root}>
       <ul>
-        <li>Цель: <span className={cl.title}>{title}</span></li>
-        <li>Описание: <span>{description}</span></li>
-        <li>Сумма: <span>{amount}</span></li>
+        <li>Цель: <span className={cl.title}>{goal?.title}</span></li>
+        <li>Описание: <span>{goal?.description}</span></li>
+        <li>Сумма: <span>{goal?.amount} руб.</span></li>
         <li className={cl.progressItem}>
-          Прогресс: <ProgressBar progress={progress}/> {progress}%
+          Прогресс: <ProgressBar progress={progress}/> {progress > 100 ? 100 : progress}%
         </li>
         <li>
-          Статус: <span className={status === "Активно" ? cl.activeStatus : ''}>{status}</span>
+          Статус: <span className={progress >= 100 ? cl.statusDone : cl.activeStatus}>{progress >= 100 ? "Выполнено" : "Активно"}</span>
         </li>
       </ul>
       <img onClick={handleRemove} className={cl.closeIcon} src="/close.png" title="удалить транзакцию"
