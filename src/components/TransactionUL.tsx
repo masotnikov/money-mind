ac32cf56a34fbb7d31b1df0d94b78701d7e52941
masotnikov <m4ksim825@ya.ru>
2024-05-12 15:37:52 +0300

    добавил memo

--- src/components/TransactionUL.tsx
+++ src/components/TransactionUL.tsx
@@ -1,28 +1,28 @@
 import cl from "./TransactionItem/TransactionItem.module.scss";
-import React, {FC} from "react";
+import React, {FC, memo} from "react";
 import {ITransaction} from "../@types/types";
 
 interface ITransactionProps {
   transaction: ITransaction;
 }
 
-const TransactionUL:FC<ITransactionProps> = ({transaction}) => {
+const TransactionUL: FC<ITransactionProps> = memo(({transaction}) => {
 
-  return (
-    <ul>
-      <li>Тип:
-        <span className={`${cl.defaultBackground} ${transaction?.type === 'Расход'
-          ? cl.redBackground
-          : cl.greenBackground}`}>
+    return (
+      <ul>
+        <li>Тип:
+          <span className={`${cl.defaultBackground} ${transaction?.type === 'Расход'
+            ? cl.redBackground
+            : cl.greenBackground}`}>
           {transaction?.type}
           </span>
-      </li>
-      <li>Категория: {transaction?.category}</li>
-      <li>Сумма: {transaction?.amount}</li>
-      <li>Дата: {transaction?.date}</li>
-      <li style={{wordWrap: 'break-word'}}>Описание: {transaction?.description}</li>
-    </ul>
-  )
-}
-
+        </li>
+        <li>Категория: {transaction?.category}</li>
+        <li>Сумма: {transaction?.amount}</li>
+        <li>Дата: {transaction?.date}</li>
+        <li style={{wordWrap: 'break-word'}}>Описание: {transaction?.description}</li>
+      </ul>
+    )
+  }
+)
 export default TransactionUL;
