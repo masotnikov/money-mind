12a6846d27a32e0aa8d61f03adedd082d9851a57
masotnikov <m4ksim825@ya.ru>
2024-05-12 15:34:28 +0300

    добавил компонент с накопительным счётом

---
+++ src/components/CurrentAccount/CurrentAccount.tsx
@@ -0,0 +1,44 @@
+import cl from "../BalanceWidget/BalanceWidget.module.scss";
+import AddReplenishForm from "../AddReplenishForm/AddReplenishForm";
+import React from "react";
+import {ITransaction} from "../../@types/types";
+import {useAddNewTransactionMutation, useGetBalanceAndExpensesQuery} from "../../API/TransactionService";
+import Loader from "../UI/loader/Loader";
+
+
+const CurrentAccount = () => {
+
+  const [replenishBalance] = useAddNewTransactionMutation();
+  // @ts-ignore
+  const {data: balanceAndExpensesData, isLoading: balanceAndExpensesLoading} = useGetBalanceAndExpensesQuery();
+
+
+  const addReplenishTransaction = async (data: ITransaction) => {
+    data.type = "Доход";
+    data.category = "Другое";
+    data.description = "Пополнение счёта";
+    data.date = "2024-03-26";
+    await replenishBalance(data);
+  }
+
+  if (balanceAndExpensesLoading) {
+    return <Loader/>
+  }
+
+
+  return (
+    <>
+      <h2>Баланс</h2>
+      <div className={cl.balanceRow}>{balanceAndExpensesData?.balance ?? 'Баланс недоступен'}
+        <AddReplenishForm onSubmit={addReplenishTransaction}></AddReplenishForm>
+      </div>
+      <hr/>
+      <h2>Расходы</h2>
+      <div>
+        <span>{balanceAndExpensesData?.expenses ?? 'Расходы недоступны'}</span>
+      </div>
+    </>
+  )
+}
+
+export default CurrentAccount;
