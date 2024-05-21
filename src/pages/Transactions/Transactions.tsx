9a9b700a2ba0684fecdba953fe5e8cfc8378cd30
masotnikov <m4ksim825@ya.ru>
2024-05-21 23:35:38 +0300

    добавил сортировку по месяцам

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -8,17 +8,17 @@ import MyButton from "../../components/UI/button/MyButton";
 import MyModal from "../../components/UI/modal/MyModal";
 import AddTransactionForm from "../../components/AddTrasnsactionForm/AddTransactionForm";
 import TransactionItem from "../../components/TransactionItem/TransactionItem";
-import {sortOptions} from "./sortOptions";
 import {IFilter} from "../../@types/types";
 
 
 const Transactions = memo(() => {
 
+    const [filter, setFilter] = useState<IFilter>({sort: '', query: '', month: ''});
     // @ts-ignore
-  const {data: transactions, isLoading, error: transactionError} = useGetAllTransactionsQuery();
-    const [filter, setFilter] = useState<IFilter>({sort: '', query: ''});
+    const {data: transactions = [], isLoading, error: transactionError} = useGetAllTransactionsQuery();
+    console.log(filter.month)
     const [modal, setModal] = useState<boolean>(false);
-    const searchedAndSortedTransactions = useTransactions(transactions, filter.query, filter.sort);
+    const searchedAndSortedTransactions = useTransactions(transactions, filter.query, filter.sort, filter.month);
     const handleCloseModal = (): void => {
       setModal(false);
     }
@@ -38,7 +38,7 @@ const Transactions = memo(() => {
           emptyMessage={"У вас нет транзакций"}
           RenderItemComponent={TransactionItem}
         >
-          <TransactionFilter filter={filter} setFilter={setFilter} sortOptions={sortOptions}/>
+          <TransactionFilter filter={filter} setFilter={setFilter}/>
           <MyButton onClick={() => setModal(true)}>Добавить транзакцию</MyButton>
         </RenderList>
         {transactionError && <h1>Произошла ошибка</h1>}
