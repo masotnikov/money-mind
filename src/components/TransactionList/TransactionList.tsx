aa3fab72efa606cf1d4409dfe8a19d3b0068cd35
masotnikov <m4ksim825@ya.ru>
2024-05-31 21:40:31 +0300

    рефакторинг компоненте

--- src/components/TransactionList/TransactionList.tsx
+++ src/components/TransactionList/TransactionList.tsx
@@ -1,24 +1,18 @@
 import cl from './TransactionList.module.scss'
-import {ITransaction} from "../../@types/types";
+import {IListProps} from "../../@types/types";
 import {FC} from "react";
 import TransactionItem from "../TransactionItem/TransactionItem";
 
-interface IListProps {
-  transactions: ITransaction[];
-  title: string;
-  emptyMessage: string;
-  children?: React.ReactNode;
-}
 
-const TransactionList: FC<IListProps> = ({title, children, transactions, emptyMessage}) => {
+const TransactionList: FC<IListProps> = ({title, children, renderList, emptyMessage}) => {
   return (
     <div className={cl.root}>
       <h3>{title}</h3>
       {children}
-      {transactions?.length === 0 && <h2>{emptyMessage}</h2>}
+      {renderList?.length === 0 && <h2>{emptyMessage}</h2>}
       <ul className={cl.list}>
-        {transactions.map(transaction => (
-            <TransactionItem transaction={transaction} key={transaction?.id}/>
+        {renderList.map(transaction => (
+          <TransactionItem transaction={transaction} key={transaction?.id}/>
         ))}
       </ul>
     </div>
