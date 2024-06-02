01b5ad4828bf939bf2ff521cbe722f8c5a14f4b4
masotnikov <m4ksim825@ya.ru>
2024-06-02 22:55:23 +0300

    добавил enum значения

---
+++ src/constants/enums.ts
@@ -0,0 +1,17 @@
+export enum TransactionType {
+  INCOME = "Доход",
+  EXPENSE = "Расход",
+}
+
+export enum TransactionCategory {
+  OTHER = "Другое",
+}
+
+export enum TransactionDescription {
+  REPLENISHMENT_SAVING_ACCOUNT = "Пополнение сберегательного счёта",
+  REPLENISHMENT_CURRENT_ACCOUNT = "Пополнение текущего счёта",
+}
+
+export enum ErrorEnum {
+  STANDARD_ERROR_MESSAGE = "Извините, произошла ошибка!"
+}
