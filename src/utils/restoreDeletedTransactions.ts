f13b26d09dbdad6a4adb0847768c284938ce3a49
masotnikov <m4ksim825@ya.ru>
2024-06-04 17:38:03 +0300

    изменён URL

--- src/utils/restoreDeletedTransactions.ts
+++ src/utils/restoreDeletedTransactions.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosResponse } from 'axios';
 import { ITransaction } from '../@types/types';
 
+const URL = 'http://money-mind-zeta.vercel.app/'
+
 const restoreDeletedTransactions = async () => {
   try {
-    const response: AxiosResponse<ITransaction[]> = await axios.get('http://localhost:3001/transactions?deleted=true');
+    const response: AxiosResponse<ITransaction[]> = await axios.get(`${URL}transactions?deleted=true`);
     const deletedTransactions: ITransaction[] = response.data;
 
     const restorePromises: Promise<AxiosResponse<ITransaction>>[] = deletedTransactions.map((transaction: ITransaction) => {
-      return axios.patch(`http://localhost:3001/transactions/${transaction.id}`, { deleted: 'false' });
+      return axios.patch(`${URL}transactions/${transaction.id}`, { deleted: 'false' });
     });
 
     await Promise.all(restorePromises);
