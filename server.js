e700ac2bca619812df15b6280ca2c2a7401f37f8
masotnikov <m4ksim825@ya.ru>
2024-06-05 22:45:16 +0300

    Удалил настройки для Vercel

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+// Listen to port
+server.listen(3001, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
