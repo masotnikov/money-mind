467225a9727ec7843437b39274e109dd718f2bfb
masotnikov <m4ksim825@ya.ru>
2024-06-05 23:14:59 +0300

    настройки для Render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = 3011; //  chose port from here like 8080, 3001
+const port = 3001; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
