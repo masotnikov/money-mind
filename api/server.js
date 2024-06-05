dd8efd88b7b209426e5ff011b4cd476c7585fdb2
masotnikov <m4ksim825@ya.ru>
2024-06-05 19:56:12 +0300

    настройки для vercel

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use('/api/', router);
 
 const port = process.env.PORT || 3001;
 console.log(port)
