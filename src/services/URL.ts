e4f4808d0fac2da51f3741736fc3b1e90f147d17
masotnikov <m4ksim825@ya.ru>
2024-06-05 18:24:41 +0300

    настройки для vercel

--- src/services/URL.ts
+++ src/services/URL.ts
@@ -1,1 +1,4 @@
-export const URL: string = `http://localhost:3001/`
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+export const URL: string = isDevelopment
+  ? process.env.REACT_APP_API_URL_DEV!
+  : process.env.REACT_APP_API_URL_PROD!;
