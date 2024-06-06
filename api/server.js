cfda9c36a3241dc75c7c97b52e33064ab7e1ab1e
masotnikov <m4ksim825@ya.ru>
2024-06-06 14:11:20 +0300

    настройки для Vercel

--- api/server.js
+++ api/server.js
@@ -1,28 +1,10 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const path = require('path');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
 
-module.exports = server
-
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json');
-// const middlewares = jsonServer.defaults();
-//
-// server.use(middlewares);
-// server.use(router);
-//
-// module.exports = server;
+module.exports = server;
