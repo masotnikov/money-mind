bc4eb99a59ced1682c2f4c3a67e9182b30a086b0
masotnikov <m4ksim825@ya.ru>
2024-06-06 14:18:16 +0300

    настройки для Vercel

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
+
 const jsonServer = require('json-server');
-const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
