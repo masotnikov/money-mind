0a5a2c055a64044bca8585ede4f7198f6af6ca26
masotnikov <m4ksim825@ya.ru>
2024-06-06 23:07:50 +0300

    настройки для Vercel!

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-const server = require('./server');
+const jsonServer = require("json-server");
+const path = require("path");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
