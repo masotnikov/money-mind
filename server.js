775598c31ce9a74a58db1b87e8af9bfa61d75c44
masotnikov <m4ksim825@ya.ru>
2024-06-08 13:52:46 +0300

    поменял server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server");
 const path = require("path");
+const express = require("express");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults({
@@ -8,6 +9,15 @@ const middlewares = jsonServer.defaults({
 
 const port = process.env.PORT || 3001;
 server.use(middlewares);
+
+// Настройка для обслуживания статических файлов
+server.use(express.static(path.join(__dirname, "build")));
+
+// Перенаправление запросов на основной маршрут для SPA
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 server.use(
   jsonServer.rewrites({
     "/api/*": "/$1",
