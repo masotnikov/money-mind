d0c9c82705dcb466dbc4daf2713f1f996b2a0eae
masotnikov <m4ksim825@ya.ru>
2024-06-09 21:58:24 +0300

    захардкожил URL

--- src/utils/restoreDeletedTransactions.ts
+++ src/utils/restoreDeletedTransactions.ts
@@ -1,14 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
 import { ITransaction } from '../@types/types';
-import {backendURL} from '../services/BackendURL'
+// import {backendURL} from '../services/BackendURL'
 
 const restoreDeletedTransactions = async () => {
   try {
-    const response: AxiosResponse<ITransaction[]> = await axios.get(`${backendURL}/transactions?deleted=true`);
+    const response: AxiosResponse<ITransaction[]> = await axios.get(`https://6664b386932baf9032abdc33.mockapi.io/transactions?deleted=true`);
     const deletedTransactions: ITransaction[] = response.data;
 
     const restorePromises: Promise<AxiosResponse<ITransaction>>[] = deletedTransactions.map((transaction: ITransaction) => {
-      return axios.put(`${backendURL}/transactions/${transaction.id}`, { deleted: 'false' });
+      return axios.put(`https://6664b386932baf9032abdc33.mockapi.io/transactions/${transaction.id}`, { deleted: 'false' });
     });
 
     await Promise.all(restorePromises);
