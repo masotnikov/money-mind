4ecce005cab31900a81d1c9cfb28b4980754f2bf
masotnikov <masotnikov11@yandex.ru>
2024-06-28 13:29:22 +0300

    поменялись импорты

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import './App.css'
+import './styles/index.scss'
 import MenuTabs from "./components/MenuTabs/MenuTabs";
 import Main from "./pages/Main/Main";
-import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import TransactionsPage from "./pages/Transactions/TransactionsPage";
 import Goals from "./pages/Goals/Goals";
 import Analytics from "./pages/Analytics/Analytics";
 import Settings from "./pages/Settings/Settings";
 import TransactionDetailsById from "./components/TransactionDetailsById/TransactionDetailsById";
-import {useEffect, useState} from "react";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 const App = () => {
-  const LIGHT_THEME: string = 'light';
-  const getThemeFromLocalStorage = () => localStorage.getItem('theme') || LIGHT_THEME;
-  const [theme, setTheme] = useState(getThemeFromLocalStorage);
-
-  useEffect(() => {
-    document.body.className = theme === LIGHT_THEME ? 'light-theme' : 'dark-theme';
-  }, [theme]);
-
 
   return (
-    <div className='App'>
+    <div className='app'>
       <HashRouter>
         <MenuTabs/>
         <Routes>
@@ -31,7 +22,7 @@ const App = () => {
           <Route path={'/transactions'} element={<TransactionsPage/>}/>
           <Route path={'/goals'} element={<Goals/>}/>
           <Route path={'/analytics'} element={<Analytics/>}/>
-          <Route path={'/settings'} element={<Settings theme={theme} setTheme={setTheme}/>}/>
+          <Route path={'/settings'} element={<Settings/>}/>
           <Route path={"*"} element={<ErrorPage/>}/>
         </Routes>
       </HashRouter>
