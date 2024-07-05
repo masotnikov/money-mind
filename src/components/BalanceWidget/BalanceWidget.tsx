84c0cd08459a5269d41f96e34b4282517940bcad
masotnikov <masotnikov11@yandex.ru>
2024-07-05 13:53:37 +0300

    переработал переключение вкладок

--- src/components/BalanceWidget/BalanceWidget.tsx
+++ src/components/BalanceWidget/BalanceWidget.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import cl from './BalanceWidget.module.scss'
 import BookmarkList from "../BookmarkList/BookmarkList";
 import CurrentAccount from "../CurrentAccount/CurrentAccount";
@@ -10,8 +10,14 @@ const bookmarks: string[] = [
   'Накопительный счёт'
 ];
 
+
 const BalanceWidget = () => {
   const [activeTab, setActiveTab] = useState<number>(0);
+
+  const bookmarkElements = [
+    <CurrentAccount/>,
+    <SavingsAccount/>
+  ]
 
   const handleTabClick = (index: number) => {
     setActiveTab(index);
@@ -20,11 +26,7 @@ const BalanceWidget = () => {
   return (
     <div className={cl.root}>
       <BookmarkList tabs={bookmarks} activeTab={activeTab} onTabClick={handleTabClick}/>
-      {activeTab === 0 ?
-        <CurrentAccount/>
-        :
-        <SavingsAccount/>
-      }
+      {bookmarkElements[activeTab]}
     </div>
   )
 }
--- src/components/ChartsWidget/ChartsWidget.tsx
+++ src/components/ChartsWidget/ChartsWidget.tsx
@@ -18,12 +18,16 @@ const bookmarks: string[] = [
 
 const ChartsWidget: FC = memo(() => {
     const DEFAULT_MONTH: string = '05'
-    const [activeTab, setActiveTab] = useState<number>(0);
     const [selectedMonth, setSelectedMonth] = useState<string>(DEFAULT_MONTH);
-
+    const [activeTab, setActiveTab] = useState<number>(0);
 
     const { data: transactions = [], isLoading, error: errorTransactions } = useGetAllTransactionsQuery({})
     const { incomeAndExpenseByMonth = [], expenseByCategory } = useDataForTable(transactions, selectedMonth);
+
+    const bookmarkElements = [
+      <GeneralAnalyticTable data={incomeAndExpenseByMonth}/>,
+      <TableExpensesByCategory data={expenseByCategory} value={selectedMonth} onChange={setSelectedMonth}/>
+    ]
 
     const handleTabClick = (index: number) => {
       setActiveTab(index);
@@ -37,15 +41,10 @@ const ChartsWidget: FC = memo(() => {
       return <ErrorMessage>{ErrorEnum.STANDARD_ERROR_MESSAGE}</ErrorMessage>;
     }
 
-
     return (
       <div className={cl.root}>
         <BookmarkList tabs={bookmarks} activeTab={activeTab} onTabClick={handleTabClick}/>
-        {activeTab === 0 ?
-          <GeneralAnalyticTable data={incomeAndExpenseByMonth}/>
-          :
-          <TableExpensesByCategory data={expenseByCategory} value={selectedMonth} onChange={setSelectedMonth}/>
-        }
+        {bookmarkElements[activeTab]}
       </div>
     )
   }
