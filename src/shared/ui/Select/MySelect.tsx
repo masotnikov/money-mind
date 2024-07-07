0415e379eac5b27bb8e13cec1e9ce288c72d5b45
masotnikov <masotnikov11@yandex.ru>
2024-07-07 14:12:13 +0300

    поправил форматирование

--- src/shared/ui/Select/MySelect.tsx
+++ src/shared/ui/Select/MySelect.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { FC } from "react";
+import React, { FC } from "react";
 import cl from './MySelect.module.scss'
-import {IOption} from "../../../@types/IOption";
+import { IOption } from "../../../@types/IOption";
 
 export interface ISelect {
   name?: string
@@ -11,10 +10,15 @@ export interface ISelect {
   onChange: (value: string) => void;
 }
 
-const MySelect: FC<ISelect> = ({ options, value, onChange, register, ...props}) => {
+const MySelect: FC<ISelect> = ({ options = [], value = '', onChange, register, ...props }) => {
   return (
-    <select {...register} {...props} className={cl.root} value={value} onChange={e => onChange(e.target.value)}>
-      {options.map((option,index: number) => (
+    <select
+      {...register}
+      {...props}
+      className={cl.root}
+      value={value}
+      onChange={e => onChange(e.target.value)}>
+      {options.map((option, index: number) => (
         <option disabled={option.disabled} key={index} value={option.value}>{option.name}</option>
       ))}
     </select>);
